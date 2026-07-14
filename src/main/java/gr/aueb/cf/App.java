f8aa0459112cdc3492d853a06aaed2d8d116ad9c
natbonn <natbon@hotmail.gr>
2026-07-14 16:59:48 +0300

    Add persistence configuration and initialize EntityManager in App class

--- src/main/java/gr/aueb/cf/App.java
+++ src/main/java/gr/aueb/cf/App.java
@@ -1,10 +1,16 @@
 package gr.aueb.cf;
 
-/**
- * Hello world!
- */
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+
 public class App {
+
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("schoolPU");
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManager em = emf.createEntityManager();
     }
 }
